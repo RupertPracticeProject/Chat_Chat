88be75cb2f691c083b92b31a99551a17b9fa137b
RupertGithub <AWS3DEMO@gmail.com>
2024-10-10 20:36:08 +0800

    socket bind

--- server.cpp
+++ server.cpp
@@ -1,5 +1,5 @@
 #define UNICODE
-#include <winsock2.h>
+#include "socket_configure.h"
 #include <Windows.h>
 #include <iostream>
 using namespace std;
@@ -16,9 +16,37 @@ int main()
     }
     else
     {
-        cout << "WSA start up success";
+        cout << "WSA start up success" << endl;
     }
     
+    SOCKET soc = socket(AF_INET, SOCK_STREAM, 0);
+    if(soc == INVALID_SOCKET)
+    {
+        cout << "Socket start up error, code : " << WSAGetLastError() << endl;
+        closesocket(soc);
+        WSACleanup();
+    }
+    else
+    {
+        cout << "Socket start up success" << endl;
+    }
+    sockaddr_in bind_addr;
+    bind_addr.sin_port = htons(3333);
+    bind_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    bind_addr.sin_family = AF_INET;
     
+    if(bind(soc, reinterpret_cast<SOCKADDR*>(&bind_addr), sizeof(bind_addr)) == SOCKET_ERROR)
+    {
+        cout << "Bind error, code : " << WSAGetLastError() << endl;
+        closesocket(soc);
+        WSACleanup();
+    }
+    else
+    {
+        cout << "Bind success" << endl;
+    }
+
+    
+
     return 0;
 }
---
+++ socket_configure.h
@@ -0,0 +1,15 @@
+#ifndef SOCKET_CONFIGURE
+#define SOCKET_CONFIGURE
+#include <winsock2.h>
+#include <string>
+using namespace std;
+class socket_configure
+{
+    public:
+        static const int port = 5555;
+        static string get_addr()
+        {
+            return "127.0.0.1";
+        }
+};
+#endif
